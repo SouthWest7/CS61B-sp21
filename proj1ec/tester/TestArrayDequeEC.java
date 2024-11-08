3c352a13597b8fc5a7dfb7e9cc73a9ca8bbc005e
am1ao <1403572259@qq.com>
2024-11-08 23:06:56 +0800

    proj1ec

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -8,48 +8,42 @@ import student.StudentArrayDeque;
 import edu.princeton.cs.algs4.StdRandom;
 
 public class TestArrayDequeEC {
-    @Test
-    public void test() {
-        StudentArrayDeque<Integer> sd = new StudentArrayDeque<>();
-        ArrayDequeSolution<Integer> ads = new ArrayDequeSolution<>();
 
-        int N = 100;
-        for (int i = 0; i < N; i += 1) {
-            int operationNumber = StdRandom.uniform(0, 4);
-            if (operationNumber == 0) {
-                // add
-                int randVal = StdRandom.uniform(0, 100);
-                int forl = StdRandom.uniform(0, 2);
-                if (forl == 0) {
-                    sd.addFirst(randVal);
-                    ads.addFirst(randVal);
-                    assertEquals("addFirst(" + randVal + ")", ads.get(0), sd.get(0));
-                } else {
-                    sd.addLast(randVal);
-                    ads.addLast(randVal);
-                    assertEquals("addLast(" + randVal + ")", ads.get(ads.size() - 1), sd.get(sd.size() - 1));
-                }
-            } else if (operationNumber == 1) {
-                // size
-                assertEquals("size()", ads.size(), sd.size());
-            } else if (!sd.isEmpty() && !ads.isEmpty()) {
-                if (operationNumber == 2) {
-                    //get
-                    int index = StdRandom.uniform(0, ads.size());
-                    assertEquals("get(" + index + ")", ads.get(index), sd.get(index));
-                } else if (operationNumber == 3) {
-                    //remove
-                    int forl = StdRandom.uniform(0, 2);
-                    if (forl == 0) {
-                        Integer x = ads.removeFirst();
-                        Integer y = sd.removeFirst();
-                        assertEquals("removeFirst()", x, y);
-                    } else {
-                        Integer x = ads.removeLast();
-                        Integer y = sd.removeLast();
-                        assertEquals("removeLast()", x, y);
-                    }
-                }
+    @Test
+    public void testDeque() {
+        StudentArrayDeque<Integer> st = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> ad = new ArrayDequeSolution<>();
+
+        int n = 5000;
+        String str = "\n";
+        for (int i = 0; i < n; i++) {
+            int opt = StdRandom.uniform(0, 4);
+            if (opt == 0) {
+                // addFirst
+                int num = StdRandom.uniform(0, 100);
+                st.addFirst(num);
+                ad.addFirst(num);
+                str += "addFirst(" + num + ")\n";
+            } else if (opt == 1) {
+                // addLast
+                int num = StdRandom.uniform(0, 100);
+                st.addLast(num);
+                ad.addLast(num);
+                str += "addLast(" + num + ")\n";
+            }
+
+            if (st.isEmpty()) {
+                continue;
+            }
+
+            if (opt == 2) {
+                // removeFirst
+                str += "removeFirst()\n";
+                assertEquals(str, ad.removeFirst(), st.removeFirst());
+            } else if (opt == 3){
+                // removeLast
+                str += "removeLast()\n";
+                assertEquals(str, ad.removeLast(), st.removeLast());
             }
         }
     }
