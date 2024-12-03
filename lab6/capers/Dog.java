1fd9559aba4e24b1da66e22beaa912ab0f3a71bc
am1ao <1403572259@qq.com>
2024-12-03 12:40:23 +0800

    lab6

--- lab6/capers/Dog.java
+++ lab6/capers/Dog.java
@@ -7,10 +7,10 @@ import static capers.Utils.*;
 /** Represents a dog that can be serialized.
  * @author TODO
 */
-public class Dog { // TODO
+public class Dog implements Serializable{ // TODO
 
     /** Folder that dogs live in. */
-    static final File DOG_FOLDER = null; // TODO (hint: look at the `join`
+    static final File DOG_FOLDER = Utils.join(CapersRepository.CAPERS_FOLDER, "dogs"); // TODO (hint: look at the `join`
                                          //      function in Utils)
 
     /** Age of dog. */
@@ -40,7 +40,9 @@ public class Dog { // TODO
      */
     public static Dog fromFile(String name) {
         // TODO (hint: look at the Utils file)
-        return null;
+        File file = new File(DOG_FOLDER, name);
+
+        return Utils.readObject(file, Dog.class);
     }
 
     /**
@@ -57,6 +59,7 @@ public class Dog { // TODO
      */
     public void saveDog() {
         // TODO (hint: don't forget dog names are unique)
+        Utils.writeObject(Utils.join(DOG_FOLDER, name), this);
     }
 
     @Override
